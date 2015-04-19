63d81a66f79950fdb9141b071eea5835e2b026c1
Ashley Ingram <ashley2504@hotmail.com>
2015-04-19 20:59:15 +0100

    Created an Article record type and associated functions
    This is an early iteration of what 'articles' (web pages) might look like.
    URIs need to be extracted to get subsequent articles, and html pages need
    to be downloaded from the web. Added a unit test project with associated
    nuget packages to test the simple (non-web based) Article code.

---
+++ Digest.Tests/ArticleTests.fs
@@ -0,0 +1,22 @@
+﻿module Digest.Tests.ArticleTests
+
+open Digest
+open FsUnit.Xunit
+open Xunit
+
+[<Fact>]
+let ``Article.GetLinks gets URI of all links in a document``() =
+    let html = "<a href=\"http://google.com\">Google</a><a href=\"http://bing.com\">Bing</a>"
+    let article = { Text = html }
+    let links = Article.ExtractLinks(article) |> Seq.map(fun l -> l.AbsoluteUri)
+    links |> Seq.length |> should equal 2
+    links |> should contain "http://google.com/"
+    links |> should contain "http://bing.com/"
+
+[<Fact>]
+let ``Article.GetLinks does not return a URI in the body of a document, just that in a href attribute``() =
+    let html = "<a href=\"http://google.com\">Google</a>. http://bing.com"
+    let article = { Text = html }
+    let links = Article.ExtractLinks(article) |> Seq.map(fun l -> l.AbsoluteUri)
+    links |> Seq.length |> should equal 1
+    links |> should contain "http://google.com/"
---
+++ Digest/Article.fs
@@ -0,0 +1,30 @@
+﻿namespace Digest
+
+open System
+open System.Net
+open System.Text.RegularExpressions
+open FSharp.Data
+open Microsoft.FSharp.Control.WebExtensions
+
+type ArticleType = { Text: string }
+type RankedArticleType = { Article: ArticleType; Rank: double }
+
+module Article =
+    
+    let private downloadFromUri uri = 
+            let webClient = new WebClient()
+            webClient.AsyncDownloadString(uri)
+    
+    let Create uri =
+        async {
+            let! content = downloadFromUri uri
+            return { Text = content }
+        }
+
+    let RankArticle article = 
+        { Article = article; Rank = 0.0 }
+
+    let ExtractLinks body = 
+        let html = HtmlDocument.Parse(body.Text)
+        let links = html.Descendants["a"] |> Seq.choose(fun e -> e.TryGetAttribute("href")) |> Seq.map(fun l -> l.Value())
+        links |> Seq.map(fun l -> new Uri(l))
