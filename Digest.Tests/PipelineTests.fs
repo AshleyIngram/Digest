c034a3aefd66a5a0c85ce361131f89b22b2e69a6
Ashley Ingram <ashley2504@hotmail.com>
2015-04-27 21:44:26 +0100

    Create an Actor for downloading articles
    First iteration of a pipeline to tie all the bits together. The separate
    parts of the system need to be connected together like an (asynchronous)
    pipeline, but with the ability to feed back into the start of the pipeline
    (so that an articles links can be fed back into the system)

---
+++ Digest.Tests/PipelineTests.fs
@@ -0,0 +1,31 @@
+﻿module Digest.Tests.PipelineTests
+
+open System
+open System.Threading
+open Digest
+open Digest.Pipeline
+open FSharp.Control
+open FsUnit.Xunit
+open Xunit
+
+type TestActor() = 
+    let mutable recievedMessages = []
+
+    member this.GetMessageCount() =
+        recievedMessages |> Seq.length
+
+    interface IActor<ArticleType> with
+        member this.Cancel() = ()
+        member this.Post(m) = recievedMessages <- m :: recievedMessages
+        
+
+[<Fact>]
+let ``The FetchArticles mailbox gets messages over time``() =
+    let rootUri = new Uri("http://google.com")
+    let actor = new Pipeline.FetchArticles()
+    let assertionActor = new TestActor()
+    actor.AddChild(assertionActor)
+    actor.Post(rootUri)
+    Thread.Sleep(TimeSpan.FromSeconds(10.0))
+    actor.Cancel()
+    assertionActor.GetMessageCount() |> should not' (equal 0)
---
+++ Digest/Pipeline.fs
@@ -0,0 +1,58 @@
+﻿module Digest.Pipeline
+
+open System
+open System.Diagnostics
+open System.Net
+
+type IActor<'a> = 
+    abstract member Cancel: unit -> unit
+    abstract member Post: 'a -> unit
+
+type Cancellable<'a> =
+    | Value of 'a
+    | Cancelled
+
+let handleFailedWebRequest (httpWebResponse: HttpWebResponse) =
+    let responseUri = httpWebResponse.ResponseUri.ToString()
+    let status = httpWebResponse.StatusCode.ToString()
+    let traceMessage = sprintf "WebException occurred: %s %s" responseUri status
+    Trace.TraceError(traceMessage)
+
+type FetchArticlesActor() = 
+    
+    let mutable children: IActor<'ArticleType> list = []
+
+    let rec mailbox = MailboxProcessor.Start(fun inbox ->
+        let rec loop() = 
+            async {
+                let! message = inbox.Receive()
+
+                match message with
+                | Cancelled -> return 0
+                | Value uri ->
+                    try 
+                        let! article = Article.Create uri
+                        let links = Article.ExtractLinks article
+                        links |> Seq.iter (fun l -> mailbox.Post(Value(l)))
+                        children |> Seq.iter (fun c -> c.Post(article))
+                        return! loop()
+                    with
+                        | :? UriFormatException as e ->
+                            Trace.TraceError(sprintf "Invalid URI: %s" e.Message)
+                            return! loop()
+                        | :? WebException as e when (e.Response :? HttpWebResponse) ->
+                            let httpWebResponse = e.Response :?> HttpWebResponse
+                            handleFailedWebRequest httpWebResponse
+                            return! loop()
+            }
+
+        loop() |> Async.Ignore
+    )
+    
+    member this.Cancel() = (this :> IActor<_>).Cancel()
+    member this.Post(v) = (this :> IActor<_>).Post(v)
+    member this.AddChild(c) = (children <- (c :: children))
+
+    interface IActor<Uri> with
+        member this.Cancel() = mailbox.Post(Cancelled)
+        member this.Post(uri) = mailbox.Post(Value uri)
