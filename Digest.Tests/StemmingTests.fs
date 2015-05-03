9adae620812325a0c62a107507005de8e79739de
Ashley Ingram <ashley2504@hotmail.com>
2015-05-03 17:16:35 +0100

    Add word stemming algorithm
    Stemming will allow recommendation algorithms to operate with greater
    accuracy, as words which mean the same thing will be treated as the same,
    rather than giving different rankings to words with the same root and the
    same meaning. In the future, this could potentially be replaced with a lemmatizer for
    more accuracy.

---
+++ Digest.Tests/StemmingTests.fs
@@ -0,0 +1,17 @@
+﻿module Digest.TextAnalysis.Tests.StemmingTests
+
+open Digest.TextAnalysis.Stemming
+open FsUnit.Xunit
+open Xunit
+
+let inline (=>) (x: string) (y: string) = x |> stem |> should equal y
+
+[<Fact>]
+let ``Words should be stemmed to a common root``() =
+    "account" => "account"
+    "accountabilities" => "account"
+    "accountability" => "account"
+    "accountable" => "account"
+    "accountant" => "account"
+    "accounted" => "account"
+    "accounting" => "account"
---
+++ Digest/Stemming.fs
@@ -0,0 +1,8 @@
+﻿module Digest.TextAnalysis.Stemming
+
+open Iveonik.Stemmers
+
+let private stemmer = new EnglishStemmer() 
+
+let stem word = 
+    stemmer.Stem(word)
