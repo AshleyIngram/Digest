c6a744402cc2c00073fa563c63152da294f6f2f8
Ashley Ingram <ashley2504@hotmail.com>
2015-05-23 15:40:51 +0100

    Create a Persistent Dictionary abstraction
    The persistent dictionary will be one of a few 'Persistent Collections'
    used to store the state of the application. The collections will be backed
    by files, so the application can restore its state should it be shut down.
    
    Conventionally this would be provided by a Database or other more formal
    storage mechanism, but I feel like having simpler collections at this
    point reduces complexity.

---
+++ Digest.Tests/PersistentCollectionsTests.fs
@@ -0,0 +1,14 @@
+﻿module Digest.Tests.PersistentCollectionsTests
+
+open System.IO
+open Digest.PersistentCollections
+open FsUnit.Xunit
+open Xunit
+
+[<Fact>]
+let ``An item added to the dictionary is available to any new dictionaries with the same id``() =
+    let dict1 = new PersistentDictionary<string, int>("testDictionary")
+    dict1.set("foo", 1)
+    let dict2 = new PersistentDictionary<string, int>("testDictionary")
+    dict2.get("foo") |> should equal 1
+    File.Delete("storage/testDictionary")
---
+++ Digest/PersistentCollections.fs
@@ -0,0 +1,29 @@
+﻿module Digest.PersistentCollections
+
+open System.Collections.Generic
+open System.IO
+open Nessos.FsPickler
+
+type PersistentDictionary<'k, 'v when 'k : equality>(id: string) =
+    let filepath = "storage/" + id
+    let pickler = FsPickler.CreateBinary()
+    
+    let load() = 
+        let fileStream = File.OpenRead(filepath)
+        pickler.Deserialize<Dictionary<'k, 'v>>(fileStream)
+
+    let underlyingDict = if (File.Exists(filepath)) then load() else new Dictionary<'k, 'v>()
+
+    let save() =
+        let fileContents = pickler.Pickle underlyingDict
+        Directory.CreateDirectory("storage") |> ignore
+        let file = File.Create(filepath)
+        file.Write(fileContents, 0, fileContents.Length)
+        file.Close()
+
+    member this.set(key: 'k, value: 'v) =
+        underlyingDict.Add(key, value) |> ignore
+        save()
+
+    member this.get(key: 'k) =
+        underlyingDict.Item(key)
