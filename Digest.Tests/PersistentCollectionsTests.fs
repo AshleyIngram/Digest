72e4a55e0b6b7e2bfdf462336fe8a428844656bc
Ashley Ingram <ashley2504@hotmail.com>
2015-07-02 15:27:50 +0100

    Add try/finally to PersistentCollections tests
    This fixes a situation where a test failure means the backing file for a
    persistent collection is not deleted. This will cause the test to fail
    subsequently, as the file still exists and will therefore cause duplicate
    key exceptions (or counts on queues to be incorrect, etc).
    
    Moving the delete call to a finally block ensures its always cleaned up
    appropriately. Ideally this would be moved to some form of disposal
    method, but this doesn't seem to exist in FsUnit.

--- Digest.Tests/PersistentCollectionsTests.fs
+++ Digest.Tests/PersistentCollectionsTests.fs
@@ -7,27 +7,33 @@ open Xunit
 
 [<Fact>]
 let ``An item added to the dictionary is available to any new dictionaries with the same id``() =
-    let dict1 = new PersistentDictionary<string, int>("testDictionary")
-    dict1.Set("foo", 1)
-    let dict2 = new PersistentDictionary<string, int>("testDictionary")
-    dict2.Get("foo") |> should equal 1
-    File.Exists("storage/testDictionary_dict") |> should equal true
-    File.Delete("storage/testDictionary_dict")
+    try
+        let dict1 = new PersistentDictionary<string, int>("testDictionary")
+        dict1.Set("foo", 1)
+        let dict2 = new PersistentDictionary<string, int>("testDictionary")
+        dict2.Get("foo") |> should equal 1
+        File.Exists("storage/testDictionary_dict") |> should equal true
+    finally
+        File.Delete("storage/testDictionary_dict")
 
 [<Fact>]
 let ``An item added to a set is available to any set with the same id``() =
-    let set1 = new PersistentSet<int>("testSet")
-    set1.Add(3)
-    let set2 = new PersistentSet<int>("testSet")
-    set2.Contains(3) |> should equal true
-    File.Exists("storage/testSet_set") |> should equal true
-    File.Delete("storage/testSet_set")
+    try
+        let set1 = new PersistentSet<int>("testSet")
+        set1.Add(3)
+        let set2 = new PersistentSet<int>("testSet")
+        set2.Contains(3) |> should equal true
+        File.Exists("storage/testSet_set") |> should equal true
+    finally
+        File.Delete("storage/testSet_set")
 
 [<Fact>]
-let ``An item added to a queue is available to any queue with the same id``()=
-    let set1 = new PersistentQueue<string>("testQueue")
-    set1.Enqueue("foo")
-    let set2 = new PersistentQueue<string>("testQueue")
-    set2.Dequeue() |>Option.get |> should equal "foo"
-    File.Exists("storage/testQueue_queue") |> should equal true
-    File.Delete("storage/testQueue_queue")
+let ``An item added to a queue is available to any queue with the same id``() =
+    try
+        let set1 = new PersistentQueue<string>("testQueue")
+        set1.Enqueue("foo")
+        let set2 = new PersistentQueue<string>("testQueue")
+        set2.Dequeue() |> Option.get |> should equal "foo"
+        File.Exists("storage/testQueue_queue") |> should equal true
+    finally
+        File.Delete("storage/testQueue_queue")
